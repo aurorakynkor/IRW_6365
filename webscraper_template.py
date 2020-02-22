c4223b26e1b8dad437621f9371da4c91ec56e90e
aurorakynkor <61165470+aurorakynkor@users.noreply.github.com>
2020-02-21 19:03:50 -0500

    Created Webscraper Template

---
+++ webscraper_template.py
@@ -0,0 +1,12 @@
+def get_IRWGoesHere_jobs (job_keyword, job_location):
+    """This function takes a web url, and scrapes jobs from a url and gets
+    all jobs posted within the last week
+
+    Parameters:
+    job_keyword: String
+    job_location: String
+
+    Returns:
+    job_data: list we can return to our db query, the only info we need is the raw number of jobs
+    """
+    return job_data
